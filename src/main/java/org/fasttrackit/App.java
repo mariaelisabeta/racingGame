c6ceba5227b980e59b92a71f98620a8a549725b9
mariaelisabeta <gasparel.emaria@gmail.com>
2017-11-13 10:43:15 +0200

    racingGame1

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -2,12 +2,44 @@ package org.fasttrackit;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+// this is a comment
+
+public class App {
+    public static void main(String[] args) {
+        Race race = new Race();
+        Track track= new Track();
+        track.length = 100.5;
+        race.track = new Track();
+
+
+        Engine engine = new Engine ();
+        engine.manufacturer = "volvo";
+
+        Car car1 = new Car();
+        car1.name = "Volvo";
+        car1.color = "black";
+        car1.fuelLevel=100;
+        car1.mileage=12.5;
+        car1.doorCount=4;
+        car1.engine = engine;
+
+        Engine engine2= new Engine();
+        engine2.manufacturer="honda";
+
+        Car car2 = new Car();
+        car2.name="Honda";
+        car2.color="red";
+        car2.fuelLevel=100;
+        car2.mileage=13.;
+        car2.doorCount=2;
+        car2.engine=engine2;
+
+
+
+
+
+
     }
 }
---
+++ src/main/java/org/fasttrackit/Race.java
@@ -0,0 +1,8 @@
+package org.fasttrackit;
+
+public class Race {
+    Track track;
+    Car firstCompetitor;
+    Car secondCompetitor;
+
+}
