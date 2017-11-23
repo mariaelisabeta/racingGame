8419ceb2fe4d9f553175b116c6cfe681cb3a5ddc
mariaelisabeta <gasparel.emaria@gmail.com>
2017-11-23 20:39:29 +0200

    simplified App.main // adica metoda main din clasa app

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -15,32 +15,7 @@ public class App {
         track.setLength(100.5);
         race.setTrack(track);
 
-        Engine engine = new Engine();
-        engine.setManufacturer("Volvo");
-
-
-        Car car1 = new Car(engine);
-        car1.setName("Volvo");
-        car1.setColor("balck");
-        car1.setFuelLevel(100.00);
-        car1.setMileage(12.5);
-        car1.setDoorCount(4);
-
-
-        Engine engine2 = new Engine();
-        engine2.setManufacturer("honda");
-
-        Car car2 = new Car(engine2);
-        car2.setName("Honda");
-        car2.setColor("red");
-        car2.setFuelLevel(100);
-        car2.setMileage(13.5);
-        car2.setDoorCount(2);
-
-
-        Vehicle vehicle = new Vehicle();
-        vehicle.accelerate(20.5);
-
+        race.start();
 
 
     }
--- src/main/java/org/fasttrackit/Race.java
+++ src/main/java/org/fasttrackit/Race.java
@@ -5,6 +5,38 @@ public class Race {
    private Car firstCompetitor;
    private Car secondCompetitor;
 
+   public void start () {
+       Engine engine = new Engine();
+       engine.setManufacturer("Volvo");
+       firstCompetitor = createCompetitor(engine, "Volvo", "red", 40, 8.5, 4);
+
+       Engine secondEngine = new Engine();
+       secondEngine.setManufacturer("Toyota");
+       secondCompetitor= createCompetitor(secondEngine, "Volvo", "black", 10.5, 9, 2);
+
+
+   }
+
+
+
+   //nu ar trebui sa avem f multi parametri intr-o metoda
+   private Car createCompetitor(Engine engine, String name, String color, double fuelLevel, double mileage, int doorcount) {
+       Car competitor = new Car (engine);
+       competitor.setName(name);
+       competitor.setColor(color);
+       competitor.setFuelLevel(fuelLevel);
+       competitor.setMileage(mileage);
+       competitor.setDoorCount(doorcount);
+
+       return competitor;
+
+
+
+
+   }
+
+
+
     public Track getTrack() {
         return track;
     }
